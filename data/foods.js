53e985eb3d58fb3692f663ce9a2449c02176ec68
Christine Tsai <rtg7bs@virginia.edu>
2022-11-12 12:16:24 -0500

    work

--- data/foods.js
+++ data/foods.js
@@ -2,10 +2,7 @@ const axios = require("axios");
 
 async function getFoods() {
   const { data } = await axios.get(`https://www.fsis.usda.gov/shared/data/EN/foodkeeper.json`);
-  for (let i = 0; i < data["sheets"][2]["data"].length; i++) {
-    console.log(data["sheets"][2]["data"][i][2]["Name"])
-  }
-  return data;
+  return data["sheets"][2]["data"];
 }
 
 module.exports = { getFoods };
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,8 @@ const path = require("path");
 const { getFoods } = require("../data/foods")
 
 const constructorMethod = (app) => {
-  app.use("/expdates/lmeats", async (req, res) => {
-    await getFoods()
-    res.render("foods/expdate", {title: "Lunch Meats", expdates: ["opened package: 3-5 days in refridgerator, 1-2 months in freezer", "unopened package: 2 weeks in refridgerator, 1-2 months in freezer"]})
-  })
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve("static/index.html"));
+  app.get("/", async(req, res) => {
+    res.render("foods/expdate", {expdates: await getFoods(), title: "test string"});
   });
   app.use("*", (req, res) => {
     res.status(404).json({ error: "Not found" });
